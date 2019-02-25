e6917e4ea4597fc23d5904c1cd0cd88f5b1c71fe
Anna.Mineeva <anechkamk@rambler.ru>
2019-02-25 15:13:44 +0300

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.*;
+
+public class HelloWorld {
+    public static void main (String [] args){
+        showMessageDialog(null,"Всем большой привет!");
+    }
+}
